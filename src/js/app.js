988aac41fc32b11d10a107e58f03f500258b2ee3
Mostafizur Rahman <44481263+shimulsql@users.noreply.github.com>
2022-02-02 13:26:17 +0600

    responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
-console.log('app script is running');
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    new Typed('.banner-typing', {
+        strings: ["Hi I'm Al Jamee", "Graphics Designer"],
+        startDelay: 1200,
+        backDelay: 3500,
+        typeSpeed: 40,
+        loop: true
+    });
+
+
+})
